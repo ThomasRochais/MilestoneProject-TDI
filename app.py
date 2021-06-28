d33df9618b8a8ab5fb850aa7ff178a11714fcb9e
Thomas Rochais <thb@sas.upenn.edu>
2021-06-27 23:18:46 -0400

    update

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request, redirect
+import numpy as np
+
+from bokeh.embed import components
+
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
-import numpy as np
-from bokeh.embed import components
 
 class StockForm(FlaskForm):
-  closing = BooleanField('Closing Price')
-  adjusted_closing = BooleanField('Adjusted Closing Price')
-  opening = BooleanField('Opening Price')
-  adjusted_opening = BooleanField('Adjusted Opening Price')
-  submit = SubmitField('Submit')
+    closing = BooleanField('Closing Price')
+    adjusted_closing = BooleanField('Adjusted Closing Price')
+    opening = BooleanField('Opening Price')
+    adjusted_opening = BooleanField('Adjusted Opening Price')
+    submit = SubmitField('Submit')
 
 from stocks import stocks
 
@@ -20,19 +22,22 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-  closing, adjusted_closing, opening = None, None, None
-  form = StockForm()
-  if form.validate_on_submit():
-    closing = form.closing.data
-    adjusted_closing = form.adjusted_closing.data
-    opening = form.opening.data
-  plot = stocks(closing=closing, adjusted_closing=adjusted_closing, opening=opening)
-  
-  script, div = components(plot)
-  return render_template('index.html', div=div, script=script, form=form)
+    closing, adjusted_closing, opening = None, None, None
+    form = StockForm()
+    if form.validate_on_submit():
+        closing = form.closing.data
+        adjusted_closing = form.adjusted_closing.data
+        opening = form.opening.data
+    plot = stocks(closing=closing, adjusted_closing=adjusted_closing, opening=opening)
+    
+    script, div = components(plot)
 
+    return render_template("index.html", 
+                            div=div, 
+                            script=script,
+                            form=form)
 
 if __name__ == '__main__':
   app.run()
