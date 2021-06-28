92dfc9587af1f25a5aff4f4743af287dbcc7c65b
Thomas Rochais <thb@sas.upenn.edu>
2021-06-27 23:47:05 -0400

    update

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template, request, redirect
-from flask_wtf import FlaskForm
-from flask_bootstrap import Bootstrap
-from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
-import numpy as np
-from bokeh.embed import components
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
