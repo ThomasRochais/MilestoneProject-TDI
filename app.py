daa20c781bc5d3eab11d19b1ca4be57c99b2a3f1
Thomas Rochais <thb@sas.upenn.edu>
2021-06-27 23:09:04 -0400

    update

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+import numpy as np
+from bokeh.embed import components
+
+class StockForm(FlaskForm):
+  closing = BooleanField('Closing Price')
+  adjusted_closing = BooleanField('Adjusted Closing Price')
+  opening = BooleanField('Opening Price')
+  adjusted_opening = BooleanField('Adjusted Opening Price')
+  submit = SubmitField('Submit')
+
+from stocks import stocks
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+bootstrap = Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  closing, adjusted_closing, opening = None, None, None
+  form = StockForm()
+  if form.validate_on_submit():
+    closing = form.closing.data
+    adjusted_closing = form.adjusted_closing.data
+    opening = form.opening.data
+  plot = stocks(closing=closing, adjusted_closing=adjusted_closing, opening=opening)
+  
+  script, div = components(plot)
+  return render_template('index.html', div=div, script=script, form=form)
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run()
