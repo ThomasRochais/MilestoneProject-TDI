14332c7b46560d87e4a3bdb421441f6c55771a0a
Thomas Rochais <thb@sas.upenn.edu>
2021-06-28 00:01:45 -0400

    update

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from flask import Flask, render_template, request, redirect
+import numpy as np
+from bokeh.embed import components
+# Pulling the stock data
+import pandas as pd
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+# api_key = os.environ['19CLJ8KCICHV7QXL']
+# url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey={}'.format(api_key)
+# r = request.get(url)
+# data = r.json()
+# df = pd.DataFrame(data)
 
 app = Flask(__name__)
 
