da2ce8352cb00e40fb425364e33a39126be2b6f5
Thomas Rochais <thb@sas.upenn.edu>
2021-06-27 23:38:38 -0400

    update

--- app.py
+++ app.py
@@ -6,37 +6,36 @@ from wtforms.validators import DataRequired
 import numpy as np
 from bokeh.embed import components
 
-class StockForm(FlaskForm):
-  closing = BooleanField('Closing Price')
-  adjusted_closing = BooleanField('Adjusted Closing Price')
-  opening = BooleanField('Opening Price')
-  adjusted_opening = BooleanField('Adjusted Opening Price')
-  submit = SubmitField('Submit')
+# from stocks import stocks
 
-from stocks import stocks
+# class StockForm(FlaskForm):
+#   closing = BooleanField('Closing Price')
+#   adjusted_closing = BooleanField('Adjusted Closing Price')
+#   opening = BooleanField('Opening Price')
+#   adjusted_opening = BooleanField('Adjusted Opening Price')
+#   submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+# app.config['SECRET_KEY'] = 'hard to guess string'
 
-bootstrap = Bootstrap(app)
+# bootstrap = Bootstrap(app)
 
-# Pulling the stock data
-import pandas as pd
-from dotenv import load_dotenv
-import os
+# # Pulling the stock data
+# import pandas as pd
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
-api_key = os.environ['19CLJ8KCICHV7QXL']
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey={}'.format(api_key)
-r = request.get(url)
-data = r.json()
-df = pd.DataFrame(data)
-
+# load_dotenv()
+# api_key = os.environ['19CLJ8KCICHV7QXL']
+# url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey={}'.format(api_key)
+# r = request.get(url)
+# data = r.json()
+# df = pd.DataFrame(data)
 
 @app.route('/')
 def index():
   return render_template('index.html')
-
 
 if __name__ == '__main__':
   app.run()
